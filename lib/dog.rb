f73bef8e3a26161952274a693381bad364b66588
Dina Hafez <dinahafez.8@gmail.com>
2018-08-29 13:46:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,7 @@
 class Dog
-  
   def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name 
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-  
 end
-
-bruno = Dog.new("Bruno")
-
-puts "This is #{bruno.name}. It is a #{bruno.breed}."
 
